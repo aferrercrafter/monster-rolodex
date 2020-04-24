8cf695b6e3e13a3f3a823913ae44260e25fff909
Arnaldo Corzo Ferrer <aferrercrafter@gmail.com>
2020-04-24 00:13:44 -0300

    Updates

---
+++ precache-manifest.c25ab23ef1bd29b80f6ab45a8ce29ce0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eccb631eae5bf036746a8b8335097c41",
+    "url": "/monster-rolodex.git/index.html"
+  },
+  {
+    "revision": "72dac99c3f2051c40856",
+    "url": "/monster-rolodex.git/static/css/main.35adad78.chunk.css"
+  },
+  {
+    "revision": "9f834d1fc4df12fb31cd",
+    "url": "/monster-rolodex.git/static/js/2.308474c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex.git/static/js/2.308474c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72dac99c3f2051c40856",
+    "url": "/monster-rolodex.git/static/js/main.dbdeca80.chunk.js"
+  },
+  {
+    "revision": "3186675f2b36f9f43159",
+    "url": "/monster-rolodex.git/static/js/runtime-main.47e8500d.js"
+  }
+]);
